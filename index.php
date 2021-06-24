536d312686dd61aa1a6816fdcc65e711e6f80e66
Shunsuke Ota <shunsukeota@ShunsukenoMacBook-Pro.local>
2021-06-24 18:22:36 +0900

    初期構成ファイルの作成

---
+++ index.php
@@ -0,0 +1,35 @@
+<!-- Ver 1.0.0 -->
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SLOT_開発</title>
+  <!-- Stylesheet -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <!-- メイン表示エリア -->
+  <div id="app">
+
+    <!-- 描画キャンバス -->
+    <canvas id="canvas"></canvas>
+
+    <!-- 描画パーツ -->
+    <div class="lever">
+
+    </div>
+
+    <!-- 情報・設定パネル -->
+    <div id="information">
+
+    </div>
+  </div>
+  <script src="main.js"></script>
+</body>
+
+</html>
